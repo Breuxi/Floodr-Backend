998cd05dcb30ccec03fe45a3b27ec760ce7560bb
breuxi <breuxi.innovations@gmail.com>
2016-06-12 11:38:35 +0200

    CORS Support

--- Rest.py
+++ Rest.py
@@ -7,6 +7,42 @@ import time
 
 app = flask.Flask(__name__)
 
+
+@app.before_request
+def option_autoreply():
+    if flask.request.method == 'OPTIONS':
+        resp = app.make_default_options_response()
+
+        headers = None
+        if 'ACCESS_CONTROL_REQUEST_HEADERS' in flask.request.headers:
+            headers = flask.request.headers['ACCESS_CONTROL_REQUEST_HEADERS']
+
+        h = resp.headers
+
+        # Allow the origin which made the XHR
+        h['Access-Control-Allow-Origin'] = flask.request.headers['Origin']
+        # Allow the actual method
+        h['Access-Control-Allow-Methods'] = flask.request.headers['Access-Control-Request-Method']
+        # Allow for 10 seconds
+        h['Access-Control-Max-Age'] = "10"
+
+        # We also keep current headers
+        if headers is not None:
+            h['Access-Control-Allow-Headers'] = headers
+
+        return resp
+
+
+@app.after_request
+def set_allow_origin(resp):
+    h = resp.headers
+
+    # Allow crossdomain for other HTTP Verbs
+    if flask.request.method != 'OPTIONS' and 'Origin' in flask.request.headers:
+        h['Access-Control-Allow-Origin'] = flask.request.headers['Origin']
+
+    return resp
+
 homematic_ip = "192.168.0.4"
 
 finish = False
@@ -34,7 +70,7 @@ def backend():
                 if value2 == "0":
                     print("Zähler 2 aktiv")
                     zeit = time.clock() - t0
-                    if zeit < 1:
+                    if zeit < 60:
                         global export
                         export = json.dumps({'message': 'Alarm'})
                         Homematic.setValue(homematic_ip, session_id, "BidCos-RF", sensor1, "STATE", "boolean", "1")
@@ -74,7 +110,6 @@ def shutdown_server():
 def api():
     global export
     resp = flask.Response(export, status=200, mimetype='application/json')
-
     return resp
 
 
